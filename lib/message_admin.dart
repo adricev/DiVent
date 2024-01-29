e2c024d6563a63ea3e0f3a4c2a10c0a83316d548
adricev <adriandiez1@gmail.com>
2024-01-29 14:19:35 +0100

    PANTALLA DE MESSAGE ADMIN CREADA, FALTA PROPRAMAR BOTÓN

--- lib/message_admin.dart
+++ lib/message_admin.dart
@@ -1,14 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MessageAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Text('Esta es la pantalla de mensaje admin'),
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.only(
+                  bottom:
+                      120.0), // Margen inferior para separar la imagen del texto
+              child: SvgPicture.asset(
+                'images/splashLogo1.svg',
+                width: 80.0,
+                height: 80.0,
+              ),
+            ),
+            Text(
+              'A continuación podrás mandarle un mensaje al administrador de la aplicación para solicitar un código y acceder a un calendario.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Asunto',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Color(0xFFD8D5FC),
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 12.0,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              maxLines: 7,
+              decoration: InputDecoration(
+                labelText: 'Mensaje',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Color(0xFFD8D5FC),
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 12.0,
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // Acciones cuando se presiona el botón
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF7268DD),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              ),
+              child: Text(
+                'Enviar mensaje',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
